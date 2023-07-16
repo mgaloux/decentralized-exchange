4bec7116987685412a38aca12f79150239a39952
Zequar <martin.galoux@epitech.eu>
2023-07-16 16:23:26 +0200

    price chart

---
+++ src/components/PriceChart.js
@@ -0,0 +1,55 @@
+import { useSelector } from "react-redux";
+import Banner from './Banner.js';
+import ReactApexChart from "react-apexcharts";
+import { options, defaultSeries } from "./PriceChart.config.js";
+import { priceChartSelector } from "../store/selectors.js";
+import { use } from "chai";
+import arrowDown from '../assets/down-arrow.svg'
+import arrowUp from '../assets/up-arrow.svg'
+
+const PriceChart = () => {
+
+    // are you connected with metamask
+    const account = useSelector(state => state.provider.account)
+    const symbols = useSelector(state => state.tokens.symbols)
+    const priceChart = useSelector(priceChartSelector)
+
+    return (
+      <div className="component exchange__chart">
+        <div className='component__header flex-between'>
+          <div className='flex'>
+  
+            <h2>{symbols && `${symbols[0]}/${symbols[1]}`}</h2>
+  
+            { priceChart && 
+                <div className='flex'>
+                <img
+                    src={priceChart.lastPriceChange == '+' ?
+                    arrowUp : arrowDown}
+                    alt={priceChart.lastPriceChange == '+' ?
+                    'Arrow Up' : 'Arrow Down'} />
+                <span className='up'>{priceChart.lastPrice}</span>
+                </div>
+            }
+            
+  
+          </div>
+        </div>
+  
+        {!account ?
+        <Banner text='Please Connect with MetaMask'></Banner>
+        :
+        <ReactApexChart
+            type="candlestick"
+            options={options}
+            series={priceChart ? priceChart.series : defaultSeries}
+            width="100%"
+            height="100%"
+        />
+        }
+  
+      </div>
+    );
+  }
+  
+  export default PriceChart;
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect'
-import { get, groupBy, reject } from 'lodash'
+import { get, groupBy, reject, maxBy, minBy, max } from 'lodash'
 import { ethers } from 'ethers'
 import moment from 'moment'
 
@@ -108,3 +108,62 @@ export const orderBookSelector = createSelector(
         
         return orders
 })
+
+// PRICE CHART /////////////////////////////////////////////////
+//
+
+export const priceChartSelector = createSelector(
+    filledOrders,
+    tokens,
+    (orders, tokens) => {
+        if (!tokens[0] || !tokens[1]) { return }
+
+        orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
+        orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+
+        // sort by timestamp (descending)
+        orders = orders.sort((a, b) => a.timestamp - b.timestamp)
+
+        orders = orders.map((o) => decorateOrder(o, tokens))
+
+        let secondLastOrder, lastOrder
+        [secondLastOrder, lastOrder] = orders.slice(orders.length - 2, orders.length)
+        const lastPrice = get(lastOrder, 'tokenPrice', 0)
+        const secondLastPrice = get(secondLastOrder, 'tokenPrice', 0)
+
+       
+        return ({
+            lastPrice,
+            lastPriceChange: (lastPrice >= secondLastPrice ? '+' : '-'),
+            series: [{
+                data: buildGraphData(orders)
+            }]
+        })
+        
+
+    }
+)
+
+const buildGraphData = (orders) => {
+    // group orders by day. startOf can also work with hours, minutes etc
+    orders = groupBy(orders, (o) => moment.unix(o.timestamp).startOf('hour').format())
+
+    const hours = Object.keys(orders)
+
+
+    return hours.map((hour) => {
+        // Fetch all order from current hour
+        const group = orders[hour]
+
+        // Calculate price values
+        const open = group[0] // first order
+        const high = maxBy(group, 'tokenPrice') // maximum price
+        const low = minBy(group, 'tokenPrice') // lowest price
+        const close = group[group.length - 1] // last order
+
+        return ({
+            x: new Date(hour), 
+            y: [open.tokenPrice, high.tokenPrice, low.tokenPrice, close.tokenPrice]
+        })
+    })
+}
