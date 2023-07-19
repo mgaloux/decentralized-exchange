e14837667f668437f0321ae17c27ea94a417e4d5
Zequar <martin.galoux@epitech.eu>
2023-07-19 16:24:05 +0200

    deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
                     <select name="networks" id="networks" value={config[chainId] ? `0x${chainId.toString(16)}` : `0`} onChange={networkHandler}>
                         <option value="0" disabled>Select Network</option>
                         <option value="0x7A69">Localhost</option>
-                        <option value="0x5">Goerli</option>
+                        <option value="0xaa36a7">Sepolia</option>
                     </select>
                 }
             </div>
